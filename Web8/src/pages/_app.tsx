dff1bc3caac317acf40531c7ca4e18415466db71
feature18 <100363745+feature18@users.noreply.github.com>
2023-11-10 22:58:06 +0300

    Add files via upload

---
+++ Web8/src/pages/_app.tsx
@@ -0,0 +1,14 @@
+import '@/styles/globals.css'
+import {ThemeProvider} from '@mui/material'
+import type { AppProps } from 'next/app'
+import {createTheme} from "@mui/material/styles";
+import { Provider } from 'react-redux';
+import {store} from "@/store/store";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return  <Provider store={store}><ThemeProvider theme={theme}>
+    <Component {...pageProps} />
+  </ThemeProvider></Provider>
+}
+
+const theme = createTheme();
